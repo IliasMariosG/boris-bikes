e695cbf280bbe4605a12502a12d467050422ca3d
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-12-03 19:42:01 +0200

    Refactor - remove block

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do
-  context 'when starting' do
     it {is_expected.to respond_to :release_bike }
-  end
   context 'when bike is working' do
     it 'releases a bike' do
       bike = subject.release_bike
