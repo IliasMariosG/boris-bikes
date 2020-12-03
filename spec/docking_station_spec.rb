b442e34c63ae7483328302182455d4eb141a778e
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-12-03 19:44:37 +0200

    Refactor - improve example content

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
     it {is_expected.to respond_to :release_bike }
-  context 'when bike is working' do
-    it 'releases a bike' do
+  context 'when a bike is released' do
+    it 'is working' do
       bike = subject.release_bike
       expect(bike).to be_working
     end
