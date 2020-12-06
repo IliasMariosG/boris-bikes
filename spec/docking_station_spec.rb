7729dbd7e5961e56376d2f4041cadfcc22362462
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-12-06 20:34:30 +0200

    Change 'it' example from 'docks a bike' to 'returns a docked bike'

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,16 +3,16 @@ require 'docking_station'
 describe DockingStation do
     it {is_expected.to respond_to :release_bike }
   context 'when a bike is released' do
-    it 'is working' do
-      bike = subject.release_bike
-      expect(bike).to be_working
-    end
+   it 'is working' do
+     bike = subject.release_bike
+     expect(bike).to be_working
+   end
   end
   
   it { is_expected.to respond_to(:dock_bike).with(1).argument }
   
   it { is_expected.to respond_to(:bike) }
-  it 'docks a bike' do
+  it 'returns a docked bike' do
     bike = Bike.new
     # subject == #<DockingStation:0x00007ffccc1715d0 @bike=#<Bike:0x00007ffccc1715f8>>
     # subject.dock_bike(bike) == #<Bike:0x00007ff35a8b9480>
