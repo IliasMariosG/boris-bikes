d8350668b831ac604e14f8df9bfa993271babe80
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2020-12-11 20:45:23 +0200

    Refactor
    It leverages the Single Responsibility Principle and the constant in place of magic numbers

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,17 +2,26 @@ require './lib/bike'
 
 class DockingStation
   attr_reader :bikes
+  DEFAULT_CAPACITY = 20
   
   def initialize
     @bikes = []
   end
   def release_bike
-    fail 'No bikes available' unless @bikes.count >= 1
+    fail 'No bikes available' unless not_empty?
     @bikes.pop
   end
   # DockingStation instance docks a Bike instance
   def dock_bike(bike)
-    fail 'Docking station full: Cannot accept more than 20 bikes' if @bikes.count >= 20
+    fail 'Docking station full: Cannot accept more than 20 bikes' if full?
     @bikes << bike # array of bikes
+  end
+
+  #
+  def not_empty?
+    @bikes.count >= 1
+  end
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
   end
 end
